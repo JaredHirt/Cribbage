4f705fae45f5409ff53653c1c7641f0170e1cf07
JaredHirt <jaredhirt@hotmail.com>
2023-02-07 10:26:56 -0800

    Added hand class and crib class

--- src/Hand/Hand.java
+++ src/Hand/Hand.java
@@ -2,7 +2,7 @@
  2 * This file is part of a solution to
  3 * CPSC 101 Group Project Team Frappucino
  4 *
- 5 * Implements a hand class
+ 5 * Implements a hand class.
  7 * @author Jared Hirt
  8 * Student Number: 230154787
  9 * @version 0.1
