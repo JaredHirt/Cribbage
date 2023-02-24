2daed38e3c28628a9b7e76006e4409a9f2c69b6f
Jared Hirt <107641541+JaredHirt@users.noreply.github.com>
2023-02-23 16:50:20 -0800

    Added random cards method

--- src/Deck/Deck.java
+++ src/Deck/Deck.java
@@ -1,5 +1,7 @@
 package Deck;
 import java.util.ArrayList;
+import java.util.Collections;
+
 public class Deck {
     ArrayList<Card> theCards;
 
@@ -13,8 +15,23 @@ public class Deck {
     /**
      * Returns a set amount of unique cards
      */
-    public Card[] returnUniqueCards(){
+    public Card[] returnUniqueCards(int numberOfCards){
+        //adding all possible numbers to an array list
+        if(52 < numberOfCards || numberOfCards < 0)
+            return new Card[0];
+        Card [] uniqueCards = new Card[numberOfCards];
+        ArrayList<Integer> listOfNumbers = new ArrayList<>();
+        for(int i = 0; i < 52; i++)
+            listOfNumbers.add(i);
 
+        //Getting the random numbers
+        for(int i = 0; i < uniqueCards.length;i++) {
+            Collections.shuffle(listOfNumbers);
+            uniqueCards[i] = theCards.get(listOfNumbers.get(0));
+            listOfNumbers.remove(0);
+        }
+
+        return uniqueCards;
     }
 
 }
