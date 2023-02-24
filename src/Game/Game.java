3bd5e1c2be06b7de9c1030ea83fc7130cb18dfae
Jared Hirt <107641541+JaredHirt@users.noreply.github.com>
2023-02-23 21:55:58 -0800

    Finished the setup for the game, beginning to make the game playable.

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -4,6 +4,7 @@ package Game;
  * Holds all the game objects and runs the game
  */
 import Deck.Deck;
+import Deck.Card;
 import Player.Player;
 import Player.AI;
 public class Game {
@@ -13,6 +14,30 @@ public class Game {
         Player player = new Player();
         AI ai = new AI();
 
+        Player pone;
+        Player dealer;
+
+        //Finding out who the dealer is
+        //Need to make a GUI for this action
+        boolean dealerFound = false;
+        Card[] cardsForDealer;
+        while(!dealerFound){
+            cardsForDealer = theDeck.returnUniqueCards(2);
+            if(cardsForDealer[0].getRank().ordinal() < cardsForDealer[1].getRank().ordinal()) {
+                dealerFound = true;
+                dealer = player;
+                pone = ai;
+            }
+            if(cardsForDealer[0].getRank().ordinal() > cardsForDealer[1].getRank().ordinal()){
+                dealerFound = true;
+                dealer = ai;
+                pone = player;
+            }
+        }
+
+        //Game is now setup, now play can begin
+        //while(true) is temporary, replace it later down to make the code more readable.
+        while(true){}
 
     }
 }
--- src/Player/Player.java
+++ src/Player/Player.java
@@ -1,6 +1,10 @@
 package Player;
 import Hand.Hand;
 import Hand.Crib;
+import Deck.Card;
+
+import java.util.ArrayList;
+
 public class Player {
     private int score;
     private boolean dealer;
@@ -13,5 +17,7 @@ public class Player {
     public Player(){
         myHand = new Hand();
     }
+
+    public void setHand(ArrayList<Card> cards){myHand.setHand(cards);}
 
 }
