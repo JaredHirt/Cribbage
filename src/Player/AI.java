d513e94945c3ea5ac2266f3b579506b6b98e5804
Jared Hirt <107641541+JaredHirt@users.noreply.github.com>
2023-02-25 00:23:31 -0800

    Added almost perfect throwing to crib AI

--- src/Player/AI.java
+++ src/Player/AI.java
@@ -1,7 +1,13 @@
 package Player;
 import Deck.Card;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+
+import Deck.Deck;
+import Game.Counting;
 import Hand.Crib;
+import Hand.Hand;
 public class AI extends Player{
     public AI(){
         super();
@@ -52,4 +58,63 @@ public class AI extends Player{
         System.out.println();
 
     }
+
+    public void optimalDiscard(){
+        ArrayList<Card> hand = getHand().getHand();
+        ArrayList<Card> subsetOfHand;
+        ArrayList<ArrayList<Card>> possibleDiscards = new ArrayList<>();
+        ArrayList<Card> discards = new ArrayList<>(hand);
+        for(int i = 0; i < hand.size(); i++)
+            for(int j = i + 1; j < hand.size();j++){
+                subsetOfHand = new ArrayList<Card>(hand);
+                subsetOfHand.remove(hand.get(j));
+                subsetOfHand.remove(hand.get(i));
+                possibleDiscards.add(subsetOfHand);
+            }
+        while(possibleDiscards.size() > 1){
+            if(countOfAllPossibleCutCards(possibleDiscards.get(0), hand) > countOfAllPossibleCutCards(possibleDiscards.get(1), hand))
+                possibleDiscards.remove(1);
+            else possibleDiscards.remove(0);
+        }
+        for(Card i: possibleDiscards.get(0))
+            discards.remove(i);
+
+        for(Card i: discards){
+            getTheCrib().getHand().add(i);
+            hand.remove(i);
+        }
+
+        setPeggingCards(new ArrayList<Card>(hand));
+
+    }
+
+    private static int countOfAllPossibleCutCards(ArrayList<Card> subset, ArrayList<Card> hand){
+        int count = 0;
+        ArrayList<Card> subsetWithCutCard;
+        Card cardToRemove = null;
+        ArrayList<Card> possibleCutCards = new ArrayList<>();
+        for(int i = 0; i < 52; i++)
+            possibleCutCards.add(Card.getCard(i));
+        while(possibleCutCards.size() > 46) {
+            for (Card i : hand)
+                for (Card j : possibleCutCards)
+                    if (i.toString().equals(j.toString()))
+                        cardToRemove = j;
+            possibleCutCards.remove(cardToRemove);
+        }
+        for(Card i: possibleCutCards){
+            subsetWithCutCard = new ArrayList<Card>(subset);
+            subsetWithCutCard.add(i);
+
+            count += Counting.points15(subsetWithCutCard);
+            count += Counting.pointsPair(subsetWithCutCard);
+            count += Counting.flush(subset);
+            if(Counting.flush(subset) > 0 && subset.get(0).getSuit() == i.getSuit())
+                count++;
+            count += Counting.knob(subset, i);
+            count += Counting.runs(subsetWithCutCard);
+        }
+        return count;
+    }
 }
+
