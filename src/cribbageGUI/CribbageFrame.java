1fcdbf6393abedcd93115e816763c4b765e224c1
Jared Hirt <107641541+JaredHirt@users.noreply.github.com>
2023-03-02 16:45:43 -0800

    Added very basic GUI with start button and then it swaps component

--- src/cribbageGUI/CribbageFrame.java
+++ src/cribbageGUI/CribbageFrame.java
@@ -1,15 +1,28 @@
 package cribbageGUI;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class CribbageFrame extends JFrame {
-    public CribbageFrame(JComponent menu){
+    public CribbageFrame(){
         super("Team Frappuccino Cribbage");
         setSize(800, 800);
         setLocation(100, 100);
-        getContentPane().add(menu);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
+    }
+
+    public void startTheGame(StartGameButton buttonToRemove){
+        remove(buttonToRemove);
+
+        //Turn this game component into the actual game, draw the cribbage board, set the pegs to 0
+        GameComponent gameComponent = new GameComponent();
+        Game rectangle = new Game(new Point(50, 50), new Point(100, 100));
+        gameComponent.addGame(rectangle);
+
+
+        this.add(gameComponent);
+        revalidate();
     }
 
 }
--- src/cribbageGUI/Main.java
+++ src/cribbageGUI/Main.java
@@ -1,5 +1,4 @@
 package cribbageGUI;
-import game.Game;
 
 import javax.swing.*;
 import java.awt.*;
@@ -8,20 +7,19 @@ public class
 Main {
     public static void main(String[] args) {
 
-        javax.swing.SwingUtilities.invokeLater(
+        SwingUtilities.invokeLater(
                 () -> startGUI()
             );
     }
 
     private static void startGUI(){
-        MenuComponent menuComponent = new MenuComponent();
-        Menu menu1 = new Menu(new Point(100, 200), new Point(600, 400), new ImageIcon("src/CribbageGUI_Images/Buttons/StartGameButton.png").getImage());
+        CribbageFrame frame = new CribbageFrame();
 
-        menuComponent.addButton(menu1);
+        StartGameButton startGameButton = new StartGameButton(new ImageIcon("src/CribbageGUI_Images/Buttons/StartGameButton.png"), frame);
 
-
-        CribbageFrame frame = new CribbageFrame(menuComponent);
+        frame.add(startGameButton, BorderLayout.CENTER);
 
     }
+
 }
 
